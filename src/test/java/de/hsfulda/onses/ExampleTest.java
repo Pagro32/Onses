fc03608c4aa6df567c33a0a8655756cd5935768b
fdai7736 <yannis.liebermann@informatik.hs-fulda.de>
2024-01-16 16:32:58 +0100

    add ExampleTest for Surefire

---
+++ src/test/java/de/hsfulda/onses/ExampleTest.java
@@ -0,0 +1,14 @@
+package de.hsfulda.onses;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleTest {
+    @Test
+    @DisplayName("Example Test")
+    public void exampleTest() {
+        int test = 1 + 1;
+        assertEquals(2, test);
+    }
+}
