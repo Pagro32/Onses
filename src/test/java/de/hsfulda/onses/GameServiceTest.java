67855c0c2c40cc8fb9c94cce91efcd836f860916
fdai7920 <pascal.grosch@informatik.hs-fulda.de>
2024-02-02 15:36:06 +0100

    Test.finalRelationsTest

--- src/test/java/de/hsfulda/onses/GameServiceTest.java
+++ src/test/java/de/hsfulda/onses/GameServiceTest.java
@@ -169,4 +169,20 @@ public class GameServiceTest {
         // assert
         assertEquals(expected, answer);
     }
+    @Test
+    @DisplayName("allRelationsTest")
+    public void allRelationsTest ()
+    {
+        // arrange
+        GameService expected = new GameService();
+        //act
+        Player input1 = expected.getGame().getPlayerService().getPlayerList().getFirst();
+        Player input2 = expected.getGame().getPlayerService().getPlayerList().getLast();
+        GameService answer1 = input1.getPlayerService().getGame().getGameService();
+        GameService answer2 = input2.getGame().getGameService();
+        // assert
+        assertEquals(expected, answer1);
+        assertEquals(expected, answer2);
+    }
+
 }
