6be9ba627edbd1f7f0cc3c929663d2b89f67815a
fdai7920 <pascal.grosch@informatik.hs-fulda.de>
2024-02-02 11:33:12 +0100

    refactoring: remove useless totalTurns 2PlayerChange

--- src/main/java/de/hsfulda/onses/services/PlayerService.java
+++ src/main/java/de/hsfulda/onses/services/PlayerService.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class PlayerService {
 
     private boolean currentTurn = true; // true --> real player, false --> Bot
-    private int totalTurns = 0;
+
     private final ArrayList<Player> playerList = new ArrayList<>();
 
     public ArrayList<Player> getPlayerList() {
@@ -15,16 +15,12 @@ public class PlayerService {
     }
     public void addPlayerToList(Player player) {
         playerList.add(player);
-        totalTurns++;
     }
-
 
     public void nextTurn()
     {
         currentTurn = !currentTurn;
     }
-
-
 
     public boolean getCurrentTurn() {
         return currentTurn;
@@ -35,12 +31,4 @@ public class PlayerService {
         return this;
     }
 
-    public int getTotalTurns() {
-        return totalTurns;
-    }
-
-    public PlayerService setTotalTurns(int totalTurns) {
-        this.totalTurns = totalTurns;
-        return this;
-    }
 }
