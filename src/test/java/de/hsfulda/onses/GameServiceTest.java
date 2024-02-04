fe79dba5156f8a31ada73bc0185e2eaa5acafe83
fdai7920 <pascal.grosch@informatik.hs-fulda.de>
2024-02-04 20:43:49 +0100

    Test.DrawByChoiceAndPlay

--- src/test/java/de/hsfulda/onses/GameServiceTest.java
+++ src/test/java/de/hsfulda/onses/GameServiceTest.java
@@ -284,6 +284,25 @@ public class GameServiceTest {
     }
 
     @Test
+    @DisplayName("DrawByChoiceForcePlay")
+    public void DrawByChoiceForcePlay() {
+
+        GameService gameService = new GameService();
+        Card input = new Card().setColor(Card.Color.RED).setValue(Card.Value.ONE);
+
+        gameService.drawCard(gameService.getGame().getDrawCardDeck().size());
+        gameService.getGame().getPlayerService().setCurrentTurn(true);
+        gameService.getGame().setLastPlayedCard(new Card().setColor(Card.Color.RED).setValue(Card.Value.SEVEN));
+        gameService.getGame().addCardToDrawCardDeck(input);
+        gameService.getGame().getPlayerService().getPlayerList().getFirst().addCardToPlayerDeck(new Card().setColor(Card.Color.BLUE).setValue(Card.Value.TWO));
+        gameService.drawCard(1);
+
+        Card answer = gameService.getGame().getPlayerService().getPlayerList().getFirst().getPlayerDeck().getLast();
+
+        assertNotEquals(input, answer);
+    }
+
+    @Test
     @DisplayName("CheckThatBotDeckHasSevenCardsAfterGameStart")
     public void CheckThatBotDeckHasSevenCardsAfterGameStart() {
         GameService gameService = new GameService();
