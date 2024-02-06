64aff1644bafd541b818b020d80c16208c8fd847
fdai7736 <yannis.liebermann@informatik.hs-fulda.de>
2024-02-06 18:53:30 +0100

    add AppController Gui

--- src/main/java/de/hsfulda/onses/Main.java
+++ src/main/java/de/hsfulda/onses/Main.java
@@ -1,10 +1,9 @@
 package de.hsfulda.onses;
 
-import de.hsfulda.onses.controllers.MainController;
 import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        Application.launch(MainController.class);
+        Application.launch(App.class);
     }
 }
