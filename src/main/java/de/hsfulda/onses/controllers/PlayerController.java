57aad8efb6518b499a2df23a8a60ef76c90cc3f0
fdai7906 <finn.kraemer@informatik.hs-fulda.de>
2024-02-08 21:28:23 +0100

    refactoring cleanup destroy Methode for PlayController

--- src/main/java/de/hsfulda/onses/controllers/PlayerController.java
+++ src/main/java/de/hsfulda/onses/controllers/PlayerController.java
@@ -55,10 +55,7 @@ public class PlayerController implements Controller {
 
     @Override
     public void destroy() {
-        int i = 0;
-
-        while(i < controllers.size()) {
-            Controller controller = controllers.get(i++);
+        for (Controller controller : controllers) {
             controller.destroy();
         }
     }
